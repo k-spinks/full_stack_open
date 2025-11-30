24f2de88727f4104db447bca780010f433f74099
Kevin <k.spinks07@gmail.com>
2025-11-29 19:44:12 -0600

    feat: refactor App component to display anecdotes with voting functionality; remove unused CSS files

--- part_1/exercises/anecdotes/src/App.jsx
+++ part_1/exercises/anecdotes/src/App.jsx
@@ -1,34 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [votes, setVotes] = useState({
+    0:0,
+    1:0,
+    2:0,
+    3:0,
+    4:0,
+    5:0,
+    6:0,
+    7:0
+  })
+
+  const [selected, setSelected] = useState(0)
+  const [highestAnecdote, setHighestAnecdote] = useState(0)
+
+  const handleClick = () => {
+    let randomNum = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomNum)
+  }
+
+  const handleUpVote = () => {
+    setVotes(prevVotes => {
+      const updatedVotes = {
+        ...prevVotes,
+        [selected]: prevVotes[selected] + 1
+      }
+
+      const newHighestAnecdote = Object.keys(updatedVotes).reduce((a,b) => updatedVotes[a] > updatedVotes[b] ? a : b)
+      setHighestAnecdote(newHighestAnecdote)
+
+      return updatedVotes
+    })
+
+  }
 
   return (
-    <>
+    <div>
+      <h1>{anecdotes[selected]} has <span>{votes[selected]} likes</span></h1>
+      <button onClick={() => handleClick()}>Next Anecdote</button>
+      <button onClick={() => handleUpVote()}>Like</button>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Anecdote with the most likes</h2>
+        <h1>{anecdotes[highestAnecdote]} has <span>{votes[highestAnecdote]} likes</span></h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
