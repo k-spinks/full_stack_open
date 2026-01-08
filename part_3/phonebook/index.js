ccec0843813376837271f9d667b0a412655cac58
Kevin <k.spinks07@gmail.com>
2026-01-07 19:31:14 -0600

    refactor: remove hardcoded persons data and update API to use Contact model

--- part_3/phonebook/index.js
+++ part_3/phonebook/index.js
@@ -1,28 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Contact = require('./models/contact')
 const app = express()
 
 let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
 ]
 
 app.use(express.json())
@@ -43,7 +25,7 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Contact.find()
 })
 
 app.get('/info', (request, response) => {
